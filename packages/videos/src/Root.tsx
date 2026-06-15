ec23eae123c46090f71ab379e6b676d09a749c22
Sergio Carracedo <hi@sergiocarracedo.es>
2026-06-15 17:05:12 +0200

    fix(videos): pass entry point to remotion render and use registerRoot

--- packages/videos/src/Root.tsx
+++ packages/videos/src/Root.tsx
@@ -1,17 +1,17 @@
-import { Composition } from 'remotion'
+import { Composition, registerRoot } from 'remotion'
 import { SecurityCheckPromo } from './SecurityCheckPromo'
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <>
-      <Composition
-        id="security-check-promo"
-        component={SecurityCheckPromo}
-        durationInFrames={750}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-    </>
+    <Composition
+      id="security-check-promo"
+      component={SecurityCheckPromo}
+      durationInFrames={750}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
   )
 }
+
+registerRoot(RemotionRoot)
